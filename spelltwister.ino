8f9e67c154b7734e6f5378050f7b8ee16ec7b373
Lucas-Leroy-Henry <lucasleroyhenry@gmail.com>
2024-07-01 10:00:06 -0700

    Revert "switched to running on a single core. oscillator stability improved"
    This reverts commit 2e722367fc0fcd412f9b5448aee62c4c5cb1a08d.

--- spelltwister.ino
+++ spelltwister.ino
@@ -38,6 +38,27 @@ LedRing* _LEDRING = &ring; // used for internal ISR stuff
 Waveformer a(true,  MUX_A, LIN_TIME_A);
 Waveformer b(false, MUX_B, LIN_TIME_B);
 
+void setup() {
+    // initialize objects
+    a.init();
+    b.init();
+    leds.begin();
+    ring.begin();
+
+    Serial.begin(9600);
+    analogReadResolution(BITS_ADC);
+}
+
+void loop() {
+    // read inputs
+    a.read();
+    b.read();
+
+    ring.update(0, 0);
+    ring.write_leds(leds);
+    leds.show();
+}
+
 repeating_timer_t pwm_timer;
 bool PwmTimerHandler(repeating_timer_t* rt);
 
@@ -51,16 +72,7 @@ void b_sync_ISR() {
     b.running = true;
 }
 
-void setup() {
-    // initialize objects
-    a.init();
-    b.init();
-    leds.begin();
-    ring.begin();
-
-    Serial.begin(9600);
-    analogReadResolution(BITS_ADC);
-
+void setup1() {
     // setup outputs!
     constexpr uint16_t max_val = (1 << BIT_DEPTH) - 1;
     constexpr uint32_t pwm_freq = CLOCK_FREQ / max_val;
@@ -89,15 +101,7 @@ void setup() {
     attachInterrupt(digitalPinToInterrupt(SIG_IN_B), b_sync_ISR, FALLING);
 }
 
-void loop() {
-    // read inputs
-    a.read();
-    b.read();
-
-    ring.update(0, 0);
-    ring.write_leds(leds);
-    leds.show();
-}
+void loop1() {}  // nothing handled here, core 1 only does the interrupt
 
 bool PwmTimerHandler(repeating_timer_t* rt) {
     a.update();
