4b6592685ec3042db48bc5ccaf0787f3eb0aec54
Lucas-Leroy-Henry <lucasleroyhenry@gmail.com>
2024-08-06 11:45:42 -0700

    python solution for brightness tables implemented

--- src/colours.h
+++ src/colours.h
@@ -14,23 +14,8 @@
 #define PURPLE    0x7F00FF
 #define BLUE      0x0000FF
 
-typedef struct HSL {
-    uint16_t h; // 0 to 359
-    uint8_t s;  // 0 to 100
-    uint8_t l;  // 0 to 100
-} HSL;
 
 #define RED_HSL (HSL){0, 100, 50}
 #define BLUE_HSL (HSL){240, 100, 50}
-
-constexpr uint32_t pack_colour(uint8_t r, uint8_t g, uint8_t b);
-constexpr uint32_t rgb_dim(uint16_t hue, uint8_t value);
-constexpr uint32_t hsl_to_packed_rgb(HSL hsl);
-constexpr uint8_t hsl_convert(float c, float t1, float t2);
-constexpr int generate_brightness_tables(uint16_t a_hue, uint16_t b_hue);
-
-#define BRIGHTNESS_TABLE_LEN 256
-extern uint32_t a_side_brightness_table[BRIGHTNESS_TABLE_LEN];
-extern uint32_t b_side_brightness_table[BRIGHTNESS_TABLE_LEN];
 
 #endif
