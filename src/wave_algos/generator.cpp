41c38af6fcc5783b70cec3b4d256f442580dee19
Lucas-Leroy-Henry <lucasleroyhenry@gmail.com>
2024-08-06 13:46:59 -0700

    bug fixes from last commit

--- src/wave_algos/generator.cpp
+++ src/wave_algos/generator.cpp
@@ -19,8 +19,8 @@ uint16_t asym_lin_map(uint16_t x, uint16_t low, uint16_t mid, uint16_t high) {
 }
 
 uint16_t waveform_generator(uint16_t shifted_acc, uint16_t shape, uint16_t ratio, uint16_t upslope, uint16_t downslope) {
-    uint16_t lin_val = (shifted_acc <= ratio)? upslope * shifted_acc : downslope * (max_x - shifted_acc);
-    uint16_t expval = exptable[linval >> bit_dff]; 
+    uint16_t linval = (shifted_acc <= ratio)? upslope * shifted_acc : downslope * (max_x - shifted_acc);
+    uint16_t expval = exptable[linval >> bit_diff]; 
     uint16_t logval = logtable[linval >> bit_diff];
     return asym_lin_map(shape, expval, linval, logval);
 }
