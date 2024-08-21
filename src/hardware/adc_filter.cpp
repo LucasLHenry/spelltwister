61badd0241934a70be57e91310568a0979d9d2e5
Lucas-Leroy-Henry <lucasleroyhenry@gmail.com>
2024-08-20 21:32:24 -0700

    crazy wacky sound
    glitches when you play an ascending interval on the keyboard, will resume it's previous pitch after a couple seconds.

--- src/hardware/adc_filter.cpp
+++ src/hardware/adc_filter.cpp
@@ -2,15 +2,21 @@
 
 ADC_Filter::ADC_Filter():
     filtered_value(0),
-    smooth_amt(5) {}
+    smooth_amt(6),
+    upsample_amt(0) {
+        low_margin = 35 << upsample_amt;
+        high_margin = 200 << upsample_amt;
+    }
 
 
 uint16_t ADC_Filter::get_next(uint16_t input) {
-    uint16_t diff = ABS_SUB(input, filtered_value);
+    uint64_t upsampled_input = input << upsample_amt;
+    // int64_t diff = input - static_cast<int32_t>(filtered_value);
+    // if (diff < 0) diff *= -1;
     // these numbers could be determined algorithmically, and can be optimized
-    if (diff < 70) smooth_amt = 6;
-    if (70 <= diff < 200) smooth_amt = 2;
-    else smooth_amt = 0;
-    filtered_value += (input - filtered_value) >> smooth_amt;
-    return filtered_value;
+    // if (diff < low_margin) smooth_amt = 6;
+    // if (low_margin <= diff < high_margin) smooth_amt = 2;
+    // else smooth_amt = 0;
+    filtered_value += (upsampled_input - filtered_value) >> smooth_amt;
+    return static_cast<uint16_t>(filtered_value >> upsample_amt);
 }
--- src/hardware/adc_filter.h
+++ src/hardware/adc_filter.h
@@ -6,8 +6,10 @@
 #define ABS_SUB(a, b) (a > b)? a - b : b - a
 
 class ADC_Filter {
-    uint16_t filtered_value;
+    uint64_t filtered_value;
     uint16_t smooth_amt;
+    uint64_t low_margin, high_margin;
+    uint16_t upsample_amt;
     public:
         ADC_Filter();
         uint16_t get_next(uint16_t input);
