47d0129156c9f1c5541d6f24cdeafb58d2a4bb1e
Lucas-Leroy-Henry <lucasleroyhenry@gmail.com>
2024-08-21 21:57:18 -0700

    filter tuned!

--- src/hardware/adc_filter.cpp
+++ src/hardware/adc_filter.cpp
@@ -1,12 +1,12 @@
 #include "adc_filter.h"
 
 ADC_Filter::ADC_Filter() {
-        upsample_amt = 16;
+        upsample_amt = 8;
         low_margin = 45 << upsample_amt;
         high_margin = 200 << upsample_amt;
         smth_lo = 0;
-        smth_mid = 2;
-        smth_hi = 5;
+        smth_mid = 1;
+        smth_hi = 3;
         filtered_value = 0;
         smooth_amt = smth_hi;
     }
@@ -15,7 +15,7 @@ ADC_Filter::ADC_Filter() {
 uint16_t ADC_Filter::get_next(uint64_t input) {
     uint64_t upsampled_input = input << upsample_amt;
     uint64_t diff = ABS_SUB(upsampled_input, filtered_value);
-    // these numbers could be determined algorithmically, and can be optimized
+
     if (diff < low_margin) {
         smooth_amt = smth_hi;
     } else if (low_margin <= diff && diff < high_margin) {
